a1c1882c6a8b0216cc339b57ac5cf4b000426524
masami-max <aizawamasami@gmail.com>
2025-12-20 11:09:03 +0900

    Remove default port value in server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 1000;
+const PORT = process.env.PORT;
 
 // ===== 画像保存先 =====
 const IMAGE_PATH = path.join(__dirname, "latest.jpg");
